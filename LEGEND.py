6de965792ce6b00cf1c0d489f3ab50b9f79c5ac3
Bgmi978829 <bgmi69140@gmail.com>
2025-01-01 16:08:52 +0530

    Update LEGEND.py

--- LEGEND.py
+++ LEGEND.py
@@ -21,7 +21,7 @@ redeem_codes_collection = db['redeem_codes']
 attack_logs_collection = db['user_attack_logs']
 
 # Bot Configuration
-TELEGRAM_BOT_TOKEN = '7542386978:AAEfkK12qlBcq7nLjI1YMbSAS8tpvC0FUtw'
+TELEGRAM_BOT_TOKEN = '7023010693:AAGZYUavjUzgifc0YtFb1f-IfWhxTLuxD2Y'
 ADMIN_USER_ID = 7209762563
 ADMIN_USER_ID = 7209762563
 COOLDOWN_PERIOD = timedelta(minutes=1) 
